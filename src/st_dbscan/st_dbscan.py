7903c1d7754571ad6407b8ab62fb7672c4082945
Soof Golan <soof@mixtiles.com>
2025-02-10 15:03:31 +0200

    ruff

--- src/st_dbscan/st_dbscan.py
+++ src/st_dbscan/st_dbscan.py
@@ -11,7 +11,6 @@ ST-DBSCAN - fast scalable implementation of ST DBSCAN
 # License: MIT
 
 import numpy as np
-import sklearn.metrics.pairwise
 from scipy.spatial.distance import pdist, squareform
 from sklearn.cluster import DBSCAN
 from sklearn.metrics.pairwise import haversine_distances
@@ -21,16 +20,16 @@ from sklearn.neighbors import NearestNeighbors
 import warnings
 
 
-class ST_DBSCAN():
+class ST_DBSCAN:
     """
     A class to perform the ST_DBSCAN clustering
     Parameters
     ----------
     spatial_eps : float, default=0.5
-        The spatial density threshold (maximum spatial distance) between 
+        The spatial density threshold (maximum spatial distance) between
         two points to be considered related.
     temporal_eps : float, default=10
-        The temporal threshold (maximum temporal distance) between two 
+        The temporal threshold (maximum temporal distance) between two
         points to be considered related.
     min_samples : int, default=5
         The number of samples required for a core point.
@@ -41,7 +40,7 @@ class ST_DBSCAN():
         ‘kulsinski’, ‘mahalanobis’, ‘matching’, ‘rogerstanimoto’, ‘sqeuclidean’,
         ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘yule’.
     n_jobs : int or None, default=-1
-        The number of processes to start -1 means use all processors 
+        The number of processes to start -1 means use all processors
     Attributes
     ----------
     labels : array, shape = [n_samples]
@@ -54,18 +53,20 @@ class ST_DBSCAN():
     and Data Mining, Portland, OR, AAAI Press, pp. 226-231. 1996
 
     Birant, Derya, and Alp Kut. "ST-DBSCAN: An algorithm for clustering spatial–temporal data." Data & Knowledge Engineering 60.1 (2007): 208-221.
-    
+
     Peca, I., Fuchs, G., Vrotsou, K., Andrienko, N. V., & Andrienko, G. L. (2012). Scalable Cluster Analysis of Spatial Events. In EuroVA@ EuroVis.
     """
 
-    def __init__(self,
-                 spatial_eps=0.5,
-                 temporal_eps=10,
-                 min_samples=5,
-                 metric='euclidean',
-                 spatial_metric=None,
-                 temporal_metric=None,
-                 n_jobs=-1):
+    def __init__(
+        self,
+        spatial_eps=0.5,
+        temporal_eps=10,
+        min_samples=5,
+        metric="euclidean",
+        spatial_metric=None,
+        temporal_metric=None,
+        n_jobs=-1,
+    ):
         self.spatial_eps = spatial_eps
         self.temporal_eps = temporal_eps
         self.min_samples = min_samples
@@ -75,11 +76,11 @@ class ST_DBSCAN():
 
     def fit(self, X):
         """
-        Apply the ST DBSCAN algorithm 
+        Apply the ST DBSCAN algorithm
         ----------
         X : 2D numpy array with
-            The first element of the array should be the time 
-            attribute as float. The following positions in the array are 
+            The first element of the array should be the time
+            attribute as float. The following positions in the array are
             treated as spatial coordinates. The structure should look like this [[time_step1, x, y], [time_step2, x, y]..]
             For example 2D dataset:
             array([[0,0.45,0.43],
@@ -91,8 +92,12 @@ class ST_DBSCAN():
         # check if input is correct
         X = check_array(X)
 
-        if not self.spatial_eps > 0.0 or not self.temporal_eps > 0.0 or not self.min_samples > 0.0:
-            raise ValueError('spatial_eps, temporal_eps, min_samples must be positive')
+        if (
+            not self.spatial_eps > 0.0
+            or not self.temporal_eps > 0.0
+            or not self.min_samples > 0.0
+        ):
+            raise ValueError("spatial_eps, temporal_eps, min_samples must be positive")
 
         n, m = X.shape
 
@@ -109,12 +114,16 @@ class ST_DBSCAN():
 
             # filter the spatial_dist matrix using the time_dist
             time_dist = squareform(time_dist)
-            dist = np.where(time_dist <= self.temporal_eps, spatial_dist, 2 * self.spatial_eps)
+            dist = np.where(
+                time_dist <= self.temporal_eps, spatial_dist, 2 * self.spatial_eps
+            )
 
-            db = DBSCAN(eps=self.spatial_eps,
-                        min_samples=self.min_samples,
-                        metric='precomputed',
-                        n_jobs=self.n_jobs,)
+            db = DBSCAN(
+                eps=self.spatial_eps,
+                min_samples=self.min_samples,
+                metric="precomputed",
+                n_jobs=self.n_jobs,
+            )
             db.fit(dist)
 
             self.labels = db.labels_
@@ -125,20 +134,24 @@ class ST_DBSCAN():
 
                 # compute with sparse matrices
                 # Compute sparse matrix für Euclidean distance
-                nn_spatial = NearestNeighbors(metric=self.spatial_metric,
-                                              radius=self.spatial_eps,
-                                              n_jobs=self.n_jobs,)
+                nn_spatial = NearestNeighbors(
+                    metric=self.spatial_metric,
+                    radius=self.spatial_eps,
+                    n_jobs=self.n_jobs,
+                )
                 nn_spatial.fit(X[:, 1:])
-                euc_sp = nn_spatial.radius_neighbors_graph(X[:, 1:],
-                                                           mode='distance')
+                euc_sp = nn_spatial.radius_neighbors_graph(X[:, 1:], mode="distance")
 
                 # Compute sparse matrix für temporal distance
-                nn_time = NearestNeighbors(metric=self.temporal_metric,
-                                           radius=self.temporal_eps,
-                                           n_jobs=self.n_jobs,)
+                nn_time = NearestNeighbors(
+                    metric=self.temporal_metric,
+                    radius=self.temporal_eps,
+                    n_jobs=self.n_jobs,
+                )
                 nn_time.fit(X[:, 0].reshape(n, 1))
-                time_sp = nn_time.radius_neighbors_graph(X[:, 0].reshape(n, 1),
-                                                         mode='distance')
+                time_sp = nn_time.radius_neighbors_graph(
+                    X[:, 0].reshape(n, 1), mode="distance"
+                )
 
                 # combine both sparse matrixes and filter by time distance matrix
                 row = time_sp.nonzero()[0]
@@ -150,10 +163,12 @@ class ST_DBSCAN():
                 dist_sp = dist_sp.tocsc()
                 dist_sp.eliminate_zeros()
 
-                db = DBSCAN(eps=self.spatial_eps,
-                            min_samples=self.min_samples,
-                            metric='precomputed',
-                            n_jobs=self.n_jobs,)
+                db = DBSCAN(
+                    eps=self.spatial_eps,
+                    min_samples=self.min_samples,
+                    metric="precomputed",
+                    n_jobs=self.n_jobs,
+                )
                 db.fit(dist_sp)
 
                 self.labels = db.labels_
@@ -166,7 +181,7 @@ class ST_DBSCAN():
         ----------
         X : 2D numpy array with
             The first element of the array should be the time (sorted by time)
-            attribute as float. The following positions in the array are 
+            attribute as float. The following positions in the array are
             treated as spatial coordinates. The structure should look like this [[time_step1, x, y], [time_step2, x, y]..]
             For example 2D dataset:
             array([[0,0.45,0.43],
@@ -175,7 +190,7 @@ class ST_DBSCAN():
             If not none the dataset is split into frames and merged aferwards
         frame_overlap : float, default=temporal_eps
             If frame_size is set - there will be an overlap between the frames
-            to merge the clusters afterwards 
+            to merge the clusters afterwards
         Returns
         -------
         self
@@ -187,15 +202,22 @@ class ST_DBSCAN():
         if frame_overlap == None:
             frame_overlap = self.temporal_eps
 
-        if not self.spatial_eps > 0.0 or not self.temporal_eps > 0.0 or not self.min_samples > 0.0:
-            raise ValueError('eps1, eps2, minPts must be positive')
+        if (
+            not self.spatial_eps > 0.0
+            or not self.temporal_eps > 0.0
+            or not self.min_samples > 0.0
+        ):
+            raise ValueError("eps1, eps2, minPts must be positive")
 
-        if not frame_size > 0.0 or not frame_overlap > 0.0 or frame_size < frame_overlap:
-            raise ValueError(
-                'frame_size, frame_overlap not correctly configured.')
+        if (
+            not frame_size > 0.0
+            or not frame_overlap > 0.0
+            or frame_size < frame_overlap
+        ):
+            raise ValueError("frame_size, frame_overlap not correctly configured.")
 
         # unique time points
-        time = len(X)#np.unique(X[:, 0]) # ?
+        time = len(X)  # np.unique(X[:, 0]) # ?
 
         labels = None
         right_overlap = 0
@@ -203,9 +225,9 @@ class ST_DBSCAN():
         exit = False
 
         for i in range(0, time, (frame_size - frame_overlap + 1)):
-            if(exit):
+            if exit:
                 break
-            for period in [X[i:i + frame_size]]:
+            for period in [X[i : i + frame_size]]:
                 frame = X[np.isin(X[:, 0], period)]
 
                 self.fit(frame)
@@ -213,24 +235,21 @@ class ST_DBSCAN():
                 # match the labels in the overlaped zone
                 # objects in the second frame are relabeled
                 # to match the cluster id from the first frame
-                if not type(labels) is np.ndarray:
+                if not isinstance(labels, np.ndarray):
                     labels = self.labels
                 else:
-                    frame_one_overlap_labels = labels[len(labels) -
-                                                      right_overlap:]
+                    frame_one_overlap_labels = labels[len(labels) - right_overlap :]
                     frame_two_overlap_labels = self.labels[0:right_overlap]
 
                     mapper = {}
                     for i in list(
-                            zip(frame_one_overlap_labels,
-                                frame_two_overlap_labels)):
+                        zip(frame_one_overlap_labels, frame_two_overlap_labels)
+                    ):
                         mapper[i[1]] = i[0]
-                    mapper[
-                        -1] = -1  # avoiding outliers being mapped to cluster
+                    mapper[-1] = -1  # avoiding outliers being mapped to cluster
 
                     # clusters without overlapping points are given new cluster
-                    ignore_clusters = set(
-                        self.labels) - set(frame_two_overlap_labels)
+                    ignore_clusters = set(self.labels) - set(frame_two_overlap_labels)
                     # recode them to new cluster value
                     if -1 in labels:
                         labels_counter = len(set(labels)) - 1
@@ -245,15 +264,14 @@ class ST_DBSCAN():
                     new_labels = np.array([mapper[j] for j in self.labels])
 
                     # delete the right overlap
-                    labels = labels[0:len(labels) - right_overlap]
+                    labels = labels[0 : len(labels) - right_overlap]
                     # change the labels of the new clustering and concat
                     labels = np.concatenate((labels, new_labels))
-                    if(len(labels) == len(X)):
+                    if len(labels) == len(X):
                         exit = True
                         break
 
-                right_overlap = len(X[np.isin(X[:, 0],
-                                              period[-frame_overlap + 1:])])
+                right_overlap = len(X[np.isin(X[:, 0], period[-frame_overlap + 1 :])])
 
-        self.labels = labels[:len(X)]
+        self.labels = labels[: len(X)]
         return self
